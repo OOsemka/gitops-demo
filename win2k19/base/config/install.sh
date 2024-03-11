20d2b6068197ea675b0ea4345e8d9af9c100e8d2
OOsemka <cjanisze@redhat.com>
2024-03-11 17:26:29 -0400

    Adding http server and windows2019

---
+++ win2k19/base/config/install.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+echo "Starting install script"
+cd /scripts
+
+KC=kubectl
+${KC} get ns kubevirt-os-images >& /dev/null
+if [ $? == 0 ]
+then
+    IMAGES_NS=kubevirt-os-images
+else
+    IMAGES_NS=openshift-virtualization-os-images
+fi
+
+MYCM=$(${KC} get configmap -o name --sort-by=metadata.creationTimestamp | awk -F / '/windows-install-scripts/ {a=$2} END{ print a }')
+echo "Using ${MYCM}"
+sed "s/WININST_CM/${MYCM}/" windows-install-vm.yaml | ${KC} apply -f -
+
+echo "Applied VM, waiting for VM to start"
+sleep 5
+vm_ready=$(${KC} get vm windows-install -o jsonpath='{.status.ready}')
+while [ "$vm_ready" != "true" ]
+do
+    sleep 10
+    vm_ready=$(${KC} get vm windows-install -o jsonpath='{.status.ready}')
+done
+
+echo "VM is started. Waiting for VMI to finish successfully."
+vmi_phase=$(${KC} get vmi windows-install -o jsonpath='{.status.phase}')
+while [ "$vmi_phase" != "Succeeded" ]
+do
+    sleep 30
+    vmi_phase=$(${KC} get vmi windows-install -o jsonpath='{.status.phase}')
+done
+
+echo "VM has finished installing"
+${KC} apply -n ${IMAGES_NS} -f clone-boot-source.yaml
+echo "Applied DataVolume to clone boot source image"
+
+sleep 5
+dv_phase=$(${KC} -n ${IMAGES_NS} get dv win2k19 -o jsonpath='{.status.phase}')
+while [ "$dv_phase" != "Succeeded" ]
+do
+    sleep 10
+    dv_phase=$(${KC} -n ${IMAGES_NS} get dv win2k19 -o jsonpath='{.status.phase}')
+    echo $dv_phase
+done
+
+echo "Cleaning up"
+${KC} delete -f windows-install-vm.yaml
+
+my_app_name=$(${KC} get cm windows-install-scripts -o jsonpath='{.metadata.labels.app\.kubernetes\.io/instance}')
+
+echo "Finished"
---
+++ win2k19/base/config/post-install.ps1
@@ -0,0 +1,59 @@
+# Create Workdir
+$BasePath = "C:\Windows\Temp\Install"
+New-item $BasePath -itemtype directory
+
+# Enable RDP
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+
+# Add RedHat to Trusted Publisher
+$CertName = "balloon.cer"
+$ExportCert = Join-Path $BasePath -ChildPath $CertName
+
+$Cert = (Get-AuthenticodeSignature "e:\Balloon\2k19\amd64\balloon.sys").SignerCertificate
+$ExportType = [System.Security.Cryptography.X509Certificates.X509ContentType]::Cert
+
+[System.IO.File]::WriteAllBytes($ExportCert, $Cert.Export($ExportType))
+Import-Certificate -FilePath $ExportCert -CertStoreLocation Cert:\LocalMachine\Trust
+
+# install Guest Agent
+msiexec /i e:\virtio-win-gt-x64.msi /qn /passive
+
+# install Qemu Tools (Drivers)
+msiexec /i e:\guest-agent\qemu-ga-x86_64.msi /qn /passive
+
+# Fix Guest Agent
+Start-Process  E:\vioserial\2k19\amd64\vioser.inf -Verb install
+
+# Enable SSH
+Add-WindowsCapability -Online -Name 'OpenSSH.Server~~~~0.0.1.0'
+Set-Service -Name sshd -StartupType 'Automatic'
+
+# Enable Networking
+
+Enable-NetAdapter -Name "Ethernet" -Confirm:$false
+
+# Get Cloud-init
+#Set-ExecutionPolicy Unrestricted
+#$Cloudinit = "CloudbaseInitSetup_Stable_x64.msi"
+#$CloudinitLocation =  Join-Path -Path $BasePath -ChildPath $Cloudinit
+#invoke-webrequest https://cloudbase.it/downloads/$Cloudinit -o $CloudinitLocation
+
+#cmd /C start /wait msiexec /i $CloudinitLocation /qn
+
+# Copy cloud-init configurations in from configmap
+
+$CloudinitConfDir = "C:\Program Files\Cloudbase Solutions\Cloudbase-Init/conf"
+
+Copy-Item -Path 'F:\cloud*.conf' -Destination $CloudinitConfDir\
+
+# Cleanup
+Remove-item $BasePath -Recurse
+
+# Remove AutoLogin
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d 0 /f
+
+# Run Sysprep and Shutdown
+
+cmd /C 'cd "C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf\" && C:\Windows\System32\Sysprep\sysprep.exe /generalize /oobe /shutdown /unattend:Unattend.xml'
+#cmd /C 'C:\Windows\System32\Sysprep\sysprep.exe /generalize /oobe /shutdown'
